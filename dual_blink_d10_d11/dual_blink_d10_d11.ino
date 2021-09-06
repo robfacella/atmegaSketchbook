e60dc8cf8a42b1916bd532c37eab7437bea727ea
Robert Facella <rob.facella@gmail.com>
2021-09-06 16:44:41 -0400

    flipFlop and delayConstant

--- dual_blink_d10_d11/dual_blink_d10_d11.ino
+++ dual_blink_d10_d11/dual_blink_d10_d11.ino
@@ -1,21 +1,28 @@
 int red = 0;
 int blue = 0;
+int dC = 100;
 void setup() {
   // put your setup code here, to run once:
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
 }
 void rt() {
-  digitalWrite(11, HIGH);
-  delay(1000);
-  digitalWrite(11, LOW);
-  delay(1000);
+  if (red == 0){
+    digitalWrite(11, HIGH);
+    delay(dC);
+    red = 1;
+  }
+  else {
+    digitalWrite(11, LOW);
+    delay(dC);
+    red = 0;
+  }
 }
 void bt() {
   digitalWrite(10, HIGH);
-  delay(1000);
+  delay(dC);
   digitalWrite(10, LOW);
-  delay(1000);
+  delay(dC);
 }
 void loop() {
   // put your main code here, to run repeatedly:
