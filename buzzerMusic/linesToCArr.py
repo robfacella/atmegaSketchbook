a3eec3ae31bd17180fbcfa829d7e545e599370a3
Robert Facella <rob.facella@gmail.com>
2021-09-08 18:34:22 -0400

    strip newLineChar

--- buzzerMusic/linesToCArr.py
+++ buzzerMusic/linesToCArr.py
@@ -8,4 +8,9 @@ oF = "megalovania.Hz.Array"
 #oF = "megalovania.ms.Array"
 with open(inF, 'r') as f:
 	lines = f.readlines()
+i = 0
+#bL = []
+for l in lines:
+	lines[i] = l.strip("\n")
+	i = i + 1
 print (lines)
