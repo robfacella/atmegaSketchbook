143a043061c42a044e1bc8478e712958a3f714e1
Robert Facella <rob.facella@gmail.com>
2021-09-08 18:52:04 -0400

    yeah, that looks about right

--- buzzerMusic/linesToCArr.py
+++ buzzerMusic/linesToCArr.py
@@ -9,8 +9,19 @@ oF = "megalovania.Hz.Array"
 with open(inF, 'r') as f:
 	lines = f.readlines()
 i = 0
-#bL = []
+newLine = ''
+betterLines = []
 for l in lines:
 	lines[i] = l.strip("\n")
+	newLine = newLine + lines[i] + ", "
+	if i%13 == 0:
+		newLine = newLine+"\n"
+		betterLines.append(newLine)
+		newLine = ''
 	i = i + 1
-print (lines)
+#print (betterLines)
+with open(oF, 'w') as f:
+	f.write('{\n')
+	for line in betterLines:
+		f.write(line)
+	f.write('\n}')
